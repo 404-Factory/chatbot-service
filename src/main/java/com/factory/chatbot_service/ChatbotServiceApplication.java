a366c21f06b7e319d74ace1512693af40b0891d1
박명환 <jackpark5872@yonsei.ac.kr>
2026-05-27 01:07:24 +0900

    fix: repository 확인 불가 문제 해결

--- src/main/java/com/factory/chatbot_service/ChatbotServiceApplication.java
+++ src/main/java/com/factory/chatbot_service/ChatbotServiceApplication.java
@@ -3,12 +3,13 @@ package com.factory.chatbot_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.factory.chatbot_service.repository")
 public class ChatbotServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotServiceApplication.class, args);
 	}
-
 }
