242adf5d1c69d8c807e6a69b65c91c161be2dcd4
박명환 <jackpark5872@yonsei.ac.kr>
2026-06-09 20:58:35 +0900

    feat: 이상분석 AI 추가

---
+++ src/main/java/com/factory/chatbot/dto/AnomalyAnalysisDto.java
@@ -0,0 +1,49 @@
+package com.factory.chatbot.dto;
+
+import java.time.Instant;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class AnomalyAnalysisDto {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+        private String equipmentName;
+        private String recipeParameter;
+        private String ruleName;
+        private String anomalyType;
+        private String detectionReason;
+        private Instant occurredTime;
+        private List<DefectDto> defects;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class DefectDto {
+        private Long lotId;
+        private String defectType;
+        private String defectCode;
+        private Instant occurredTime;
+        private Instant detectedTime;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private String analysisResult;
+    }
+}
