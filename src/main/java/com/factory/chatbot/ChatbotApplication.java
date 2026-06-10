ef051639853cac97a97f898e4f8d5a5ec93b9e6b
박명환 <jackpark5872@yonsei.ac.kr>
2026-06-11 00:51:51 +0900

    feat: kafka 관련 추가

--- src/main/java/com/factory/chatbot/ChatbotApplication.java
+++ src/main/java/com/factory/chatbot/ChatbotApplication.java
@@ -5,10 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.factory.chatbot.infrastructure.repository")
+@EntityScan(basePackages = "com.factory.chatbot")
 public class ChatbotApplication {
 
     public static void main(String[] args) {
